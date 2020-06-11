55faf84473b4354f9249a2e18c159862286171ac
Александр <sasha.mirchev@gmail.com>
2020-06-11 00:39:29 -0700

    Updates

---
+++ precache-manifest.129b36d7d7883df24f8e852f91c2833e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b128d39acb95c1152e74bbdb95ba6832",
+    "url": "/canban/index.html"
+  },
+  {
+    "revision": "5eb72fe477a19ce23663",
+    "url": "/canban/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5eb72fe477a19ce23663",
+    "url": "/canban/static/js/2.78d06965.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/canban/static/js/2.78d06965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22a92ec1cb445d5a0559",
+    "url": "/canban/static/js/main.47352c8c.chunk.js"
+  },
+  {
+    "revision": "30a7dba4b6f7ae23dc7a",
+    "url": "/canban/static/js/runtime-main.92700b2d.js"
+  }
+]);
